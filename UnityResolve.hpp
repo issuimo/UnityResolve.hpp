025218a52ffc6475df45ab79af056959c99be425
遂沫 <1992724048@qq.com>
2023-12-21 20:34:45 +0800

    Add files via upload

--- UnityResolve.hpp
+++ UnityResolve.hpp
@@ -73,7 +73,7 @@ public:
 							return static_cast<RType*>(pMethod);
 						}
 					}
-					next: continue;
+				next: continue;
 				}
 
 				if (args.empty()) for (auto pMethod : methods) if (pMethod->name == name) return static_cast<RType*>(pMethod);
@@ -82,12 +82,12 @@ public:
 		}
 
 		template<typename RType>
-		auto GetFieldValue(void* obj, const std::string& name) -> RType {
+		auto GetValue(void* obj, const std::string& name) -> RType {
 			return *reinterpret_cast<RType*>(reinterpret_cast<uintptr_t>(obj) + Get<Field>(name)->offset);
 		}
 
 		template<typename RType>
-		auto SetFieldValue(void* obj, const std::string& name, RType value) -> void {
+		auto SetValue(void* obj, const std::string& name, RType value) -> void {
 			return *reinterpret_cast<RType*>(reinterpret_cast<uintptr_t>(obj) + Get<Field>(name)->offset) = value;
 		}
 
@@ -274,7 +274,7 @@ public:
 						if ((field = Invoke<void*>("il2cpp_class_get_fields", pClass, &iter))) {
 							const auto pField = new Field{ .fieldinfo = field, .name = Invoke<const char*>("il2cpp_field_get_name", field), .type = new Type{.address = Invoke<void*>("il2cpp_field_get_type", field)}, .klass = pAClass, .offset = Invoke<int>("il2cpp_field_get_offset", field), .static_field = false, .vTable = nullptr };
 							int        tSize{};
-							pField->static_field = pField->offset  <= 0;
+							pField->static_field = pField->offset <= 0;
 							pField->type->name = Invoke<const char*>("il2cpp_type_get_name", pField->type->address);
 							pField->type->size = -1;
 							pAClass->fields.push_back(pField);
@@ -302,7 +302,7 @@ public:
 										if ((field = Invoke<void*>("il2cpp_class_get_fields", i_class, &iter))) {
 											const auto pField = new Field{ .fieldinfo = field, .name = Invoke<const char*>("il2cpp_field_get_name", field), .type = new Type{.address = Invoke<void*>("il2cpp_field_get_type", field)}, .klass = pAClass, .offset = Invoke<int>("il2cpp_field_get_offset", field), .static_field = false, .vTable = nullptr };
 											int        tSize{};
-											pField->static_field = pField->offset  <= 0;
+											pField->static_field = pField->offset <= 0;
 											pField->type->name = Invoke<const char*>("il2cpp_type_get_name", pField->type->address);
 											pField->type->size = -1;
 											pAClass->fields.push_back(pField);
@@ -406,7 +406,7 @@ public:
 										if ((field = Invoke<void*>("mono_class_get_fields", iClass, &iter))) {
 											const auto pField = new Field{ .fieldinfo = field, .name = Invoke<const char*>("mono_field_get_name", field), .type = new Type{.address = Invoke<void*>("mono_field_get_type", field)}, .klass = pAClass, .offset = Invoke<int>("mono_field_get_offset", field), .static_field = false, .vTable = nullptr };
 											int        tSize{};
-											pField->static_field = pField->offset  <= 0;
+											pField->static_field = pField->offset <= 0;
 											pField->type->name = Invoke<const char*>("mono_type_get_name", pField->type->address);
 											pField->type->size = Invoke<int>("mono_type_size", pField->type->address, &tSize);
 											pAClass->fields.push_back(pField);
@@ -1478,7 +1478,7 @@ public:
 
 			auto GetComponent() -> Component* {
 				static Method* method;
-				if (!method) method = Get("UnityEngine.CoreModule.dll")->Get("GameObject")->Get<Method>("GetComponent",{ "System.Type" });
+				if (!method) method = Get("UnityEngine.CoreModule.dll")->Get("GameObject")->Get<Method>("GetComponent", { "System.Type" });
 				if (method) return method->Invoke<Component*>(this);
 				throw std::logic_error("nullptr");
 			}
