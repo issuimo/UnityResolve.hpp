c81347fc3a5a71106a42e458f2533c0a835b5d5b
遂沫 <1992724048@qq.com>
2024-02-25 00:07:06 +0800

    Update UnityResolve.hpp

--- UnityResolve.hpp
+++ UnityResolve.hpp
@@ -1384,8 +1384,7 @@ public:
 				return rs;
 			}
 
-			template <typename T>
-			static auto New(const Class* kalss, const std::uintptr_t size) -> Array<T>* {
+			static auto New(const Class* kalss, const std::uintptr_t size) -> Array* {
 				if (mode_ == Mode::Il2Cpp) return UnityResolve::Invoke<Array*, void*, std::uintptr_t>("il2cpp_array_new", kalss->classinfo, size);
 				return UnityResolve::Invoke<Array*, void*, void*, std::uintptr_t>("mono_array_new", pDomain, kalss->classinfo, size);
 			}
@@ -1400,8 +1399,7 @@ public:
 
 			auto ToArray() -> Array<Type>* { return pList; }
 
-			template <typename T>
-			static auto New(const Class* kalss, const std::uintptr_t size) -> List<T>* {
+			static auto New(const Class* kalss, const std::uintptr_t size) -> List* {
 				auto pList = new List<Type>();
 				pList->pList = Array<Type>::New(kalss, size);
 				pList->size = size;
